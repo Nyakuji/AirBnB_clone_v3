f22b12064c8e12549c59fe69b8897291e659308d
philip Opiyo <opiyophilipo@gmail.com>
2024-02-23 14:57:05 +0300

    added a fix on a test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -85,17 +85,32 @@ class TestBaseModel(unittest.TestCase):
         tic = datetime.now()
         inst1 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
+        
+        print("tic:", tic)
+        print("toc:", toc)
+        print("inst1.created_at:", inst1.created_at)
+        
+        self.assertGreaterEqual(inst1.created_at, tic)
+        self.assertLessEqual(inst1.created_at, toc)
+        
         time.sleep(1e-4)
+        
         tic = datetime.now()
         inst2 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
+        
+        print("tic:", tic)
+        print("toc:", toc)
+        print("inst2.created_at:", inst2.created_at)
+        
+        self.assertGreaterEqual(inst2.created_at, tic)
+        self.assertLessEqual(inst2.created_at, toc)
+        
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
         self.assertNotEqual(inst1.updated_at, inst2.updated_at)
-
+    
     def test_uuid(self):
         """Test that id is a valid uuid"""
         inst1 = BaseModel()
